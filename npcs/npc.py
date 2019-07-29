36cb670b866c2ef15f2eaf5512bbb3db5f730e1f
Laker Newhouse <lakernewhouse@gmail.com>
2019-07-28 19:39:20 -0700

    Added npc.py and quest.py

---
+++ npcs/npc.py
@@ -0,0 +1,40 @@
+import globals
+import input
+import output
+
+# The NPC stores three pieces of data. The name is the NPC's name. The opener is what the NPC says no matter what. Quests is an optional array that contains Quest objects. Example:
+# name = "Barton the Grizzled"
+# opener = "Traveller, I have struggled to find mushrooms as of late."
+# quests = [Quest( ... ), Quest( ... ), ...]
+
+class NPC:
+
+    def __init__(self, name, opener, quests):
+        self.name = name
+        self.opener = opener
+        self.quests = quests
+
+    def getQuests(self):
+        quests = []
+        for i in self.quests:
+            if i.condition(globals.player):
+                quests.append(i)
+        return quests
+
+    def getName(self):
+        return self.name if len(self.getQuests()) == 0 else self.name + " *"
+
+    def activate(self):
+        output.proclaim(self.name + ":")
+        output.say(self.opener)
+
+        quests = self.getQuests()
+        if len(quests) == 0:
+            return
+
+        output.proclaim("Quests:")
+        choice = input.inputFromOptions("quest", ["back"] + quests, lambda quest: quest if quest == "back" else str(quest.getName()))
+        if choice == "back":
+            return
+
+        choice.activate()
---
+++ npcs/quest.py
@@ -0,0 +1,53 @@
+import globals
+import input
+import output
+
+# Quest holds all the information to give and complete an arbitrary quest in game.
+# openerText is the quest description when an NPC first offers it.
+# incompleteText is the dialogue for if the player returns to the NPC before completing the quest.
+# completeText is the dialogue for when the player returns to the NPC and has completed the quest.
+# completionCheck is a function that determines whether the player has completed the quest. If the quest involves collecting items, then this must also delete the items from the player's inventory if the completion succeeds (use creature.checkAndRemove(itemName, number)).
+# rewards is a function that gives the player the rewards promised.
+# condition is a loot object.
+# given is a boolean that stores whether the player has accepted the quest yet
+# autoGive is a boolean that tells the quest whether to automatically jump to given = True if the condition is met. We can't just set given equal to True, because that could mess up future checks on whether the quest is in the players quest log etc.
+
+# Once the player completes the quest, we run completionCheck = lambda: False. That way, the player never sees completed quests again.
+
+class Quest:
+
+    def __init__(self, name, openerText, incompleteText, completeText, completionCheck, rewards, condition = lambda player: True, autoGive = False):
+        self.name = name
+        self.openerText = openerText
+        self.incompleteText = incompleteText
+        self.completeText = completeText
+        self.completionCheck = completionCheck
+        self.rewards = rewards
+        self.condition = condition
+
+        self.given = False
+        self.autoGive = autoGive
+
+    def getName(self):
+        return self.name if not self.given else self.name + " (in progress)"
+
+    def activate(self):
+        output.proclaim(self.name)
+
+        if self.autoGive:
+            self.given = True
+
+        if not self.given:
+            output.proclaim(self.openerText)
+            choice = input.inputFromOptions("quest", ["accept", "decline"])
+            if choice == "accept":
+                self.given = True
+            return
+
+        if self.completionCheck(globals.player):
+            output.proclaim(self.completeText)
+            output.exclaim("You have completed the quest " + self.name + "!")
+            self.rewards.activate()
+            self.condition = lambda player: False
+        else:
+            output.proclaim(self.incompleteText)
